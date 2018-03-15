77d7a0e971b11e3b942335c61f755554f0ca8dcc
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-03-15 10:01:52 -0300

    FIXME: Handle with service-worker request

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,8 @@ self.addEventListener('fetch', function(event) {
   let pedido = event.request
   let promiseResposta = caches.open('ceep-imagens').then(cache => {
     return cache.match(pedido)
-  }).then(resposta => {
+  }).then(resposptaCache => {
+    let resposta = resposptaCache ? resposptaCache : fetch(pedido)
     return resposta
   })
   event.respondWith(promiseResposta)
