a80816a5e810fce726fe1ae4b394e10ba79f81b4
Álvaro Fernando <alvarofernandoms@gmail.com>
2018-03-19 17:55:10 -0300

    Handling app update

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,5 @@
+let versao = 7
+
 let arquivos = [
   "/",
   "css/estilos.css",
@@ -34,8 +36,12 @@ let arquivos = [
 
 self.addEventListener('install', function() {
   console.log('Instalou, vey!')
-  caches.open('ceep-arquivos').then(cache => {
+  caches.open('ceep-arquivos-' + versao).then(cache => {
     cache.addAll(arquivos)
+      .then(function() {
+        caches.delete('ceep-arquivos-' + (versao - 1))
+        caches.delete('ceep-arquivos')
+      })
   })
 })
 
